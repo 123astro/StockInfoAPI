02fd0a357b8bb0efb2494367ed424e1741636321
Keith Benjamin <kjb528i@yahoo.com>
2022-03-08 20:11:35 -0500

    added StockController with functional getmapping for a stock ticker

--- src/main/java/com/careerdevs/stockinfoapi/contollers/StockController.java
+++ src/main/java/com/careerdevs/stockinfoapi/contollers/StockController.java
@@ -1,9 +1,32 @@
 package com.careerdevs.stockinfoapi.contollers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/query") // endpoint
 
 public class StockController {
+
+    @Autowired
+    private Environment env;
+
+    private String alpha_Vantage_Endpoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY" +
+            "&symbol=";
+
+
+    @GetMapping("/{stock}")
+        public Object getStock(RestTemplate restTemplate, @PathVariable("stock") String stockTicker) {
+            String apikey = env.getProperty("ALPHA_KEY");
+            return restTemplate.getForObject(alpha_Vantage_Endpoint + stockTicker + "&interval=5min&outputsize" +
+                            "=full&apikey=" + apikey,
+                    Object.class);
+
+    }
 }
